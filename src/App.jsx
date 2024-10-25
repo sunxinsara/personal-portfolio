c049d631f92740181cf74d181e41f925ae2ba72c
”sunxin“ <sunxin@sheca.com>
2024-10-26 00:22:02 +0100

    change logo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-
+      
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/cat.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Software Developer"];
+  const toRotate = [ "Software Developer", "Software Developer", "Software Developer" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,8 +56,17 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Xina`} <span className="txt-rotate" data-rotate='[ "Software Developer", "Software Developer", "Software Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p></p>
+                <h1>{`Hi! I'm Xina`}</h1>
+                <h2> <span className="txt-rotate" dataPeriod="800"><span className="wrap">{text}</span></span></h2>
+                  <ul>
+                    <li>Seeking SDE roles in Ireland, expecting to graduate with a Software Design degree from Technology University of Shannon in 2024.</li>
+                    <li>Over three years of experience in software development, primarily in JAVA.</li>
+                    <li>Implemented multiple security products, adapting quickly to market needs.</li>
+                    <li>Experienced in hybrid cloud environments with a smooth transition to AWS.</li>
+                    <li>Proficient in Docker virtualization and Dockerfile creation.</li>
+                    <li>Experienced with Kubernetes orchestration and writing resource manifests.</li>
+                    <li>Good exposure to the AGILE methodology of working.</li>
+                </ul>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
@@ -65,9 +74,9 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                <div className={isVisible ? "animate__animated " : ""}>
                   <img src={headerImg} alt="Header Img"/>
-                </div>}img
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
